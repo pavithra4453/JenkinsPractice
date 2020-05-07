ffdf889b22548b13368474ecd7b860cfc98a5431
Pavithra G <pavithra.g01@ad.infosys.com>
2020-05-07 12:29:38 +0530

    Adding PerfectSquare program

---
+++ PerfectSquare.java
@@ -0,0 +1,12 @@
+
+
+public class PerfectSquare {
+
+	public static void main(String[] args) {
+		int number=41;
+		System.out.println(Math.sqrt(number));
+		System.out.println(Math.floor(Math.sqrt(number)));
+
+	}
+
+}
